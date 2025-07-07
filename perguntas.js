83500180655a116699dcbeee618606db11d98022
EmanuelyChaves <emanuely.chaves.santos@escola.pr.gov.br>
2025-07-07 11:36:54 -0300

    kjsnjkhfikkm

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+    filme,
+    Qual foi o primeiro filme de terror em cores,
+    A maldição de frankenstein
+)
+
+criaCartao(
+    desenho,
+    Qual era o nome da boneca do Sítio do Pica-Pau Amarelo,
+    Emília
+)
+criaCartao(
+    matemática,
+    Quantos minutos a em 2 horas,
+    120 minutos
+)
