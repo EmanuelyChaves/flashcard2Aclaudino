56c5ad4155462c8bb070dcc6ab3ac4b72ffebef3
EmanuelyChaves <emanuely.chaves.santos@escola.pr.gov.br>
2025-08-11 08:56:01 -0300

    nsbjbsjjhnmjhhh

--- perguntas.js
+++ perguntas.js
@@ -1,16 +1,33 @@
 criaCartao(
-    filme,
-    Qual foi o primeiro filme de terror em cores,
-    A maldição de frankenstein
+    'filme',
+    'Qual foi o primeiro filme de terror em cores',
+   ' A maldição de frankenstein'
 )
 
 criaCartao(
-    desenho,
-    Qual era o nome da boneca do Sítio do Pica-Pau Amarelo,
-    Emília
+   ' desenho',
+    'Qual era o nome da boneca do Sítio do Pica-Pau Amarelo',
+   ' Emília'
+)
+criaCartao( 
+    'matemática',
+   ' Quantos minutos a em 2 horas',
+   ' 120 minutos'
+)
+
+criaCartao(
+    'Português',
+    'Qual o coletivo de peixe?,'
+    'cardume'
+)
+
+criaCartao(
+    'Bíblia',
+    'Quantas pragas foram enviadas para o Egito?',
+    '10'
 )
 criaCartao(
-    matemática,
-    Quantos minutos a em 2 horas,
-    120 minutos
+    'Filme',
+    'Do que era feito o sapatinho da Cinderela?',
+    'cristal',
 )
